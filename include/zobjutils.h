a9f31a7d1606b407e706b492bd9239714a6def65
Neirn <neirny@gmail.com>
2025-05-14 13:49:55 -0700

    Update zobjutils.h comments
    Now more accurate to current behavior

--- include/zobjutils.h
+++ include/zobjutils.h
@@ -4,17 +4,17 @@
 #include "global.h"
 #include "modding.h"
 
-// Repoints F3DZEX2 command to be relative to binary's location in RAM.
+// Repoints single F3DZEX2 command to be relative to newBase.
 //
-// assumes zobj points to the start of the binary and that segmented addresses are relative to it.
+// assumes zobj points to the start of the binary and that the target segmented addresses are relative to it.
 RECOMP_IMPORT("mm_recomp_zobj_utils", void ZobjUtils_repointGfxCommand(u8 zobj[], u32 commandOffset, u8 targetSegment, const void *newBase));
 
-// Repoints F3DZEX2 display list to be relative to object's location in RAM.
+// Repoints all F3DZEX2 to be relative to newBase.
 //
-// assumes zobj points to the start of the binary and that segmented addresses are relative to it.
+// assumes zobj points to the start of the binary and that the target segmented addresses are relative to it.
 RECOMP_IMPORT("mm_recomp_zobj_utils", void ZobjUtils_repointDisplayList(u8 zobj[], u32 displayListStartOffset, u8 targetSegment, const void *newBase));
 
-// Repoints FlexSkeleton to be relative to binary's location in RAM
+// Repoints FlexSkeleton to be relative to be relative to newBase.
 //
 // assumes zobj points to the start of the binary and that segmented addresses are relative to it.
 RECOMP_IMPORT("mm_recomp_zobj_utils", void ZobjUtils_repointFlexSkeleton(u8 zobj[], u32 skeletonHeaderOffset, u8 targetSegment, const void *newBase));
