0293a030c6d2954e9f1e8cdd6e8d21b572afc773
Neirn <neirny@gmail.com>
2025-05-14 11:43:34 -0700

    Update readU32 and writeU32

--- src/zobjutils.c
+++ src/zobjutils.c
@@ -8,12 +8,18 @@
 
 #define LOCAL_ARRAY_BYTE_SIZE(a) (sizeof(a) / sizeof(a[0]))
 
-u32 readU32(const u8 array[], u32 offset) {
-    return *(u32 *)(&array[offset]);
+u32 readU32(const u8 array[], u32 offset){
+    return (u32)(array[offset + 0]) << 24 |
+           (u32)(array[offset + 1]) << 16 |
+           (u32)(array[offset + 2]) << 8 |
+           (u32)(array[offset + 3]);
 }
 
 void writeU32(u8 array[], u32 offset, u32 value) {
-    *(u32 *)(&array[offset]) = value;
+    array[offset + 0] = (value & 0xFF000000) >> 24;
+    array[offset + 1] = (value & 0x00FF0000) >> 16;
+    array[offset + 2] = (value & 0x0000FF00) >> 8;
+    array[offset + 3] = (value & 0x000000FF);
 }
 
 void repointGfxCommand(u8 zobj[], u32 commandOffset, u8 targetSegment, u32 newBaseAddress) {
